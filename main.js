8c07b96df1dca65f773730c480e1528719ecf516
Dmitry Rogozik <qa.dimon@yandex.ru>
2022-02-07 09:23:53 +0300

    main

---
+++ main.js
@@ -0,0 +1,9 @@
+const date = require('./date')
+
+function foo() {
+    const dt = date.bar('D MMMM yyyy hh:mm');
+    console.log('Hello World of the QA Automation');
+    console.log(dt)
+}
+
+foo();
